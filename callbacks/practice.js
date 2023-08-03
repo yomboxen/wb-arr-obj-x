428df03697541115b1369341955139d6d5a169fc
Luke Hardy <lukeclaytonhardy@gmail.com>
2023-08-03 15:36:38 -0600

    this is great

--- callbacks/practice.js
+++ callbacks/practice.js
@@ -27,7 +27,9 @@
   Then invoke the callback function, passing in the first element in the array as it's argument.
 */
 
-// Code Here
+function first (arr, cb){
+  cb(arr[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -45,7 +47,9 @@ first(names, function (firstName) {
   Then invoke the callback, passing in the last element in the array as the argument.
 */
 
-//Code Here
+function last(arr, cb){
+  cb(arr[arr.length - 1])
+}
 
 // Do not edit the code below.
 last(names, function (lastName) {
@@ -61,7 +65,9 @@ last(names, function (lastName) {
   Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 */
 
-//Code Here
+function multiply(num1, num2, cb){
+    cb(num1 * num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function (answer) {
